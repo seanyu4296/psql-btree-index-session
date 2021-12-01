ee21bddda6dc2079e5f34d5f9917aeb9149e92bd
Sean Yu <seanyu.dev@gmail.com>
2021-12-01 12:36:35 +0800

    simplify some queries

--- 4/0-refresh-token-queries.sql
+++ 4/0-refresh-token-queries.sql
@@ -1,8 +1,7 @@
 -- queries to get refrehs token
-explain (analyze, buffers, costs, verbose) SELECT cat.id, cat.linked_account_token_id, cat.access_token, cat.refresh_token, cat.access_token_expiration_timestamp, cat.refresh_token_expiration_timestamp, cat.created, cat.updated, cat.refresh_remaining, cat.expiration_timestamp, cat.channel_metadata, lat.id, lat.business_id, lat.customer_id, lat.channel_code, lat.status, lat.type, lat.linked_account_token_details_id, lat.unlinked_account_token_id, lat.metadata, lat.device_fingerprint, lat.channel_reference_id, lat.created, lat.updated
+explain (analyze, buffers, costs, verbose) SELECT *
 FROM channel_account_tokens_bank_accounts as cat
-    JOIN linked_account_tokens as lat ON ( cat.linked_account_token_id = lat.id )
-WHERE cat.refresh_token_expiration_timestamp <= (now() + interval '2 day') AND cat.refresh_token_expiration_timestamp <> '0001-01-01 00:00:00' AND lat.status <> 'ACCOUNTS_UNLINKED' AND lat.status <> 'EXPIRED' AND lat.status <> 'FAILED' AND ( cat.refresh_remaining <> -1 OR lat.status <> 'EXPIRING' )
+WHERE cat.refresh_token_expiration_timestamp <= (now() + interval '2 day') AND cat.refresh_token_expiration_timestamp <> '0001-01-01 00:00:00'
 LIMIT 500
 
 
